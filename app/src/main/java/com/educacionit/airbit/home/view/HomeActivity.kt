853ff173a6223251330c62cc8a99dd872b510e6e
Jonathan Aranguri <arangurim@gmail.com>
2024-07-10 19:08:26 -0300

    feat: Map initial implementation

--- app/src/main/java/com/educacionit/airbit/home/view/HomeActivity.kt
+++ app/src/main/java/com/educacionit/airbit/home/view/HomeActivity.kt
@@ -1,26 +1,35 @@
 package com.educacionit.airbit.home.view
 
-import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.educacionit.airbit.R
 import com.educacionit.airbit.entities.Room
 import com.educacionit.airbit.home.contract.HomeContract
 import com.educacionit.airbit.home.model.HomeRepository
 import com.educacionit.airbit.home.presenter.HomePresenterImpl
-import com.educacionit.airbit.reservation.view.ReservationActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.AdvancedMarkerOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 
-class HomeActivity : AppCompatActivity(), HomeContract.HomeView {
+class HomeActivity : AppCompatActivity(), HomeContract.HomeView, OnMapReadyCallback {
     private lateinit var homePresenter: HomeContract.HomePresenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initPresenter()
+        configureViews()
+    }
 
-        val intent = Intent(this, ReservationActivity::class.java)
-        startActivity(intent)
+    private fun configureViews() {
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.main_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun initPresenter() {
@@ -51,5 +60,23 @@ class HomeActivity : AppCompatActivity(), HomeContract.HomeView {
 
     override fun centerUserInMap(userLocation: LatLng) {
         // TODO: Implement this later
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        val initialPosition = LatLng(0.0, 0.0)
+
+        val textView = TextView(this)
+        textView.text = "Hello!!"
+        textView.setBackgroundColor(Color.BLACK)
+        textView.setTextColor(Color.YELLOW)
+
+        val marker: Marker? = googleMap.addMarker(
+            AdvancedMarkerOptions()
+                .position(initialPosition)
+                .iconView(textView)
+        )
+
+
     }
 }
