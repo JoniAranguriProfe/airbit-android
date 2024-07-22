22b25deaa75399db60aae749cdded7b90c09190b
Jonathan Aranguri <arangurim@gmail.com>
2024-07-22 19:21:43 -0300

    feat: Map zoom level in 15.4

--- app/src/main/java/com/educacionit/airbit/base/common/MapsManager.kt
+++ app/src/main/java/com/educacionit/airbit/base/common/MapsManager.kt
@@ -23,7 +23,7 @@ object MapsManager {
     fun centerUserInMap(googleMap: GoogleMap, userLocation: LatLng) {
         val cameraPosition = CameraPosition.Builder()
             .target(userLocation) // Sets the center of the map to Mountain View
-            .zoom(17f)            // Sets the zoom
+            .zoom(15.4f)             // Sets the zoom
             .bearing(90f)         // Sets the orientation of the camera to east
             .tilt(30f)            // Sets the tilt of the camera to 30 degrees
             .build()              // Creates a CameraPosition from the builder
